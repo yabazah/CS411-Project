6c76016a9861199d044f0754a6411b4af92bfad5
Yara Abazah <abazahey@gmail.com>
2022-11-17 10:35:54 -0500

    testing js files

---
+++ test/app.js
@@ -0,0 +1,18 @@
+import './app.css';
+import header from './header';
+import card from './card';
+import buttons from './buttons';
+
+function app() {
+  return (
+    <div className="app">
+
+      <header/>
+      <card/>
+      <buttons/>
+      
+    </div>
+  );
+}
+
+export default app;
