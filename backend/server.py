fdb1a3d87338c7ba075ca24e6083063982a76d3a
jackhultstrand <98711153+jackhultstrand@users.noreply.github.com>
2022-12-12 16:53:13 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from project import dataebay, dataamz , mergearray
+from flask_cors import CORS
 
 # contact between endpoints 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/members")
+@app.route("/")
 
 def arraypush(): 
    return{"array": mergearray(dataamz,dataebay)}
+
+@app.route("/receiver", methods=["POST"])
+def postME():
+ data = request.get_json()
+ data = jsonify(data)
+ return data
 
 if __name__ == "__main__": 
     app.run(debug=True)
